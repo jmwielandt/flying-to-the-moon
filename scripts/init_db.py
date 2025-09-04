6fe3e4a0b9750f9945c0ecd41d9e9f05604ccfba
jmwielandt <github.com.guiding139@passmail.net>
2025-09-04 12:44:35 -0400

    update init_db script

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -7,14 +7,16 @@ from src.db.connect import connect_db
 from src.logs import config_logger
 
 config_logger(app_config.misc.log_level)
-con = connect_db(app_config.db.host, app_config.db.port, app_config.db.name)
+con = connect_db()
 
 r.db_create(app_config.db.name)
 
-if "flights" in r.table_list().run(con):
-    logging.warning("table 'flights' already exists")
+tables_list: list[str] = r.table_list().run(con)
+
+if "flights" in tables_list:
+    logging.warning("table 'flights' already exist")
 else:
     r.table_create("flights").run(con)
-
+    logging.info("table flights created")
 
 con.close()
